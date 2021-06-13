37aa563d16426207cb7931b288c0733f2a4a684a
d4got10 <izotov.da@students.dvfu.ru>
2021-06-13 11:43:36 +1000

    Added Matvey's datastructures

---
+++ Сoursework_Server/DataStructures/Matvey/HashTable.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace _4._FSD__5_HT
+{
+    public class List<T> where T : IEquatable<T>
+    {
+        public class Node<U> where U : IEquatable<U>
+        {
+            public Node<U> next;
+            public U data;
+            public Node<U> prev;
+
+            public Node()
+            {
+                next = this;
+                data = default;
+                prev = this;
+            }
+            public Node(U d)
+            {
+                next = this;
+                data = d;
+                prev = this;
+            }
+        }
+        Node<T> head;
+        public List()
+        {
+            head = null;
+        }
+        public bool IsFindData(T d)
+        {
+            if (head != null)
+            {
+                Node<T> current = head;
+                if (current.data.Equals(d))
+                    return true;
+                current = current.next;
+                while (current != head)
+                {
+                    if (current.data.Equals(d))
+                        return true;
+                    current = current.next;
+                }
+                return false;
+            }
+            else return false;
+        }
+        public void AddNode(T d)
+        {
+            if (head != null)
+            {
+                if (!IsFindData(d))
+                {
+                    Node<T> current = head;
+                    current = current.next;
+                    while (current != head)
+                        current = current.next;
+                    var nodeToAdd = new Node<T>(d);
+                    nodeToAdd.next = current;
+                    nodeToAdd.prev = current.prev;
+                    current.prev.next = nodeToAdd;
+                    current.prev = nodeToAdd;
+                }
+            }
+            else
+            {
+                Node<T> nodeToAdd = new Node<T>(d);
+                head = nodeToAdd;
+            }
+        }
+        public bool DeleteNode(T d)
+        {
+            if (head != null)
+            {
+                if (head.data.Equals(d))
+                {
+                    if (head.next == head)
+                    {
+                        head = null;
+                        return true;
+                    }
+                    else
+                    {
+                        Node<T> current = head.next;
+                        current.prev = head.prev;
+                        head.prev.next = current;
+                        head = current;
+                        return true;
+                    }
+                }
+                else
+                {
+                    var current = head.next;
+                    while (current != head)
+                    {
+                        if (current.data.Equals(d))
+                        {
+                            Node<T> temp = current.next;
+                            temp.prev = current.prev;
+                            current.prev.next = temp;
+                            return true;
+                        }
+                        current = current.next;
+                    }
+                }
+
+            }
+            return false;
+        }
+        public void ShowInfo()
+        {
+            if (head != null)
+            {
+                Node<T> current = head;
+                do
+                {
+                    Console.Write(current.data + " ");
+                    current = current.next;
+                } while (current != head);
+            }
+            else Console.WriteLine("_NULL_");
+        }
+    }
+    public class HashTable<T> where T : IEquatable<T>
+    {
+        public delegate int HashFunction(T key, int size);
+        public readonly HashFunction Function;
+
+        public int size;
+        public List<T>[] table;
+        public HashTable(HashFunction function, int s)
+        {
+            Function = function;
+            if (s < 1) throw new Exception("Хеш таблица пуста");
+            table = new List<T>[s];
+            size = s;
+            for (int i = 0; i < size; i++)
+            {
+                table[i] = new List<T>();
+            }
+        }
+        public void ShowHT()
+        {
+            for (int i = 0; i < size; i++)
+            { 
+                table[i].ShowInfo();
+                Console.WriteLine('\n');
+            }
+        }
+        public void AddElem(T d)
+        {
+            table[Function(d, size)].AddNode(d);
+        }
+        public void DeletElem(T d)
+        {
+            if (table[Function(d, size)].DeleteNode(d))
+            {
+                Console.WriteLine("Элемент удалён\n");
+                ShowHT();
+                Console.WriteLine('\n');
+            }
+            else
+            {
+                Console.WriteLine("Элемент не удалён, т. к. не найден\n");
+                ShowHT();
+                Console.WriteLine('\n');
+            }
+        }
+        public void Search(T d)
+        {
+            int n = Function(d, size);
+            if (table[n].IsFindData(d))
+            {
+                Console.WriteLine("Элемент " + d + " найден в строке " + Function(d, size) + '\n');
+            }
+            else
+            {
+                Console.WriteLine("Элемент " + d + " НЕ НАЙДЕН\n");
+            }
+        }
+    }
+}
---
+++ Сoursework_Server/DataStructures/Matvey/Tree.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace AVL_Tree
+{
+    public class Tree<TKey> where TKey : IComparable
+    {
+        private int HeightElem(Node<TKey> elem)
+        {
+            if (elem != null)
+                return elem.Height;
+            else
+                return 0;
+        }
+        private int BFactor (Node<TKey> elem)
+        {
+            return HeightElem(elem.Right) - HeightElem(elem.Left);
+        }
+        private void FixedHeight(Node<TKey> elem)
+        {
+            int left_h = HeightElem(elem.Left);
+            int right_h = HeightElem(elem.Right);
+            if (left_h > right_h)
+                elem.Height = left_h + 1;
+            else
+                elem.Height = right_h + 1;
+        }
+        private Node<TKey> RotateRight(Node<TKey> elem)
+        {
+            Node<TKey> p_elem = elem.Left;
+            elem.Left = p_elem.Right;
+            p_elem.Right = elem;
+            FixedHeight(elem);
+            FixedHeight(p_elem);
+            return p_elem;
+        }
+        private Node<TKey> RotateLeft(Node<TKey> elem)
+        {
+            Node<TKey> p_elem = elem.Right;
+            elem.Right = p_elem.Left;
+            p_elem.Left = elem;
+            FixedHeight(elem);
+            FixedHeight(p_elem);
+            return p_elem;
+        }
+        private Node<TKey> Balance(Node<TKey> elem)
+        {
+            FixedHeight(elem);
+            if (BFactor(elem) == 2)
+            {
+                if (BFactor(elem.Right) < 0)
+                    elem.Right = RotateRight(elem.Right);
+                return RotateLeft(elem);
+            }
+            if (BFactor(elem) == -2)
+            {
+                if (BFactor(elem.Left) > 0)
+                    elem.Left = RotateLeft(elem.Left);
+                return RotateRight(elem);
+            }
+            return elem;
+        }
+        public Node<TKey> Insert(Node<TKey> elem, TKey key)
+        {
+            if (elem == null)
+                return new Node<TKey>(key);
+            var help = new Node<TKey>(key);
+            if (help < elem)
+                elem.Left = Insert(elem.Left, key);
+            else
+                elem.Right = Insert(elem.Right, key);
+            return Balance(elem);
+        }
+        public Node<TKey> Find(Node<TKey> elem, TKey key)
+        {
+            if (elem == null) return elem;
+            var help = new Node<TKey>(key);
+            if (elem < help)
+                return Find(elem.Right, key);
+            else if (elem > help)
+                return Find(elem.Left, key);
+            return elem;
+        }
+        private Node<TKey> FindMax(Node<TKey> elem)
+        {
+            if (elem.Right != null)
+                return FindMax(elem.Right);
+            else
+                return elem;
+        }
+        private Node<TKey> RemoveMax(Node<TKey> elem)
+        {
+            if (elem.Right == null)
+                return elem.Left;
+            elem.Right = RemoveMax(elem.Right);
+            return Balance(elem);
+        }
+        public Node<TKey> Remove(Node<TKey> elem, TKey key)
+        {
+            if (elem == null) return null;
+            var help = new Node<TKey>(key);
+            if (help < elem)
+                elem.Left = Remove(elem.Left, key);
+            else if (help > elem)
+                elem.Right = Remove(elem.Right, key);
+            else
+            {
+                Node<TKey> right = elem.Right;
+                Node<TKey> left = elem.Left;
+                elem.Right = null;
+                elem.Left = null;
+                if (right == null) return left;
+                Node<TKey> max = FindMax(left);
+                max.Left = RemoveMax(left);
+                max.Right = right;
+                return Balance(max);
+            }
+            return Balance(elem);
+        }
+        private void Print(Node<TKey> elem, int tabs)
+        {
+            if (elem == null) return;
+            tabs += 5;
+            Print(elem.Right, tabs);
+
+            for (int i = 0; i < tabs; i++) Console.Write(' ');
+            Console.WriteLine(elem.Key);
+
+            Print(elem.Left, tabs);
+            tabs -= 5;
+            return;
+        }
+        public void Print(Node<TKey> elem)
+        {
+            int tabs = 0;
+            Print(elem, tabs);
+        }
+    }
+}
